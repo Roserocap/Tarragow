7d79b9260f4bb9e330285964fad8bd75e0f36771
Sergio <111180145+Roserocap@users.noreply.github.com>
2026-09-26 21:31:03 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -138,11 +138,13 @@ function changeLang(lang) {
     document.querySelector('.footer-risk-title').textContent = t.riskTitle;
     document.querySelector('.footer-risk-text').textContent = t.riskText;
 
-    // Запускаем авто-перевод через TerragowTranslate
+       // Запускаем авто-перевод через TerragowTranslate
     if (window.TerragowTranslate) {
         window.TerragowTranslate.setLang(lang);
-    
+    }
 }
+
+
 /* ===== THEME =====
    При переключении темы:
    1) сохраняем её в localStorage
